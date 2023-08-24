a20204acf853ae3a8d3a46946a8fef6354a470bb
Abdul Al Mamun <124201619+mamunCSE64@users.noreply.github.com>
2023-08-24 19:46:26 +0600

    Update Banker's Algorithm.cpp

--- Banker's Algorithm.cpp
+++ Banker's Algorithm.cpp
@@ -7,50 +7,60 @@ using namespace std;
 
 int main()
 {        
-    int n,R1,R2,R3;
-    cin>>n>>R1>>R2>>R3;
-    vector<pair<pair<int,int>,int>> maxN,allocR,currentR,available;
-    for(int i=0;i<n;i++){
-        int a,b,c; cin>>a>>b>>c;
-        maxN.pb({{a,b},c});
+    int P,R;
+    cin>>P>>R;
+    vector<int> iR(R);
+    for(int i=0;i<R;i++) cin>>iR[i];
+    vector<int> maxN[P],allocR[P],currentR[P],available[P];
+    for(int i=0;i<P;i++){
+        for(int j=0;j<R;j++){
+            int x; cin>>x; 
+            maxN[i].pb(x);
+        }
     }
-    int r1=0,r2=0,r3=0;       
-    for(int i=0;i<n;i++){
-        int a,b,c; cin>>a>>b>>c;
-        allocR.pb({{a,b},c});
-        r1+=a,r2+=b,r3+=c;
+    vector<int> r(R);     
+    for(int i=0;i<P;i++){
+        for(int j=0;j<R;j++){
+            int x;cin>>x;
+            allocR[i].pb(x);
+            r[j]+=x;
+        }
     } 
-    for(int i=0;i<n;i++){
-        int x,y,z;
-        x=maxN[i].fi.fi-allocR[i].fi.fi;
-        y=maxN[i].fi.se-allocR[i].fi.se;
-        z=maxN[i].se-allocR[i].se;
-        currentR.pb({{x,y},z});
+    for(int i=0;i<P;i++){
+        for(int j=0;j<R;j++){
+            int d=maxN[i][j]-allocR[i][j];
+            currentR[i].pb(d);
+        }         
     }
-    int nR1,nR2,nR3;
-    nR1=R1-r1;
-    nR2=R2-r2;
-    nR3=R3-r3;
+    vector<int> nR(R);
+    for(int i=0;i<R;i++){
+        nR[i]=iR[i]-r[i];
+    }     
     int count=0;
-    vector<int> a(n,1);    
-    cout << R1 << " " << R2 << " " << R3 << endl;
-    while(count<n){
-        int x=1;
-        for(int i=0;i<n;i++){
-            if(a[i]){
-                if(currentR[i].fi.fi<=nR1 and 
-                    currentR[i].fi.se<=nR2 
-                    and currentR[i].se<=nR3){
-
-                    nR1+=allocR[i].fi.fi;
-                    nR2+=allocR[i].fi.se;
-                    nR3+=allocR[i].se;
-                    a[i]=0,count++,x=0;
-                    cout << "Safe State : P " << i << endl;
+    vector<int> done(P,0);  
+    cout << "Initial Resource : " << endl;
+    for(int i=0;i<R;i++) cout << iR[i] << " "; cout << endl;
+    while(count<P){
+        bool deadlock=0;
+        for(int i=0;i<P;i++){
+            if(done[i]==0){
+                bool b=1;
+                for(int j=0;j<R;j++){
+                    if(currentR[i][j]>nR[j]) {
+                        b=0;break;
+                    }
                 }
+                if(b){
+                    for(int j=0;j<R;j++){
+                        nR[j]+=allocR[i][j];
+                    }
+                    done[i]=1,deadlock=1,count++;
+                    cout << "Safe State : P " << i << endl;
+                }                 
             }
         }
-        if(x){ cout << "DeadLock Created " << endl; return 0;}
+        if(deadlock==0){ cout << "DeadLock Created " << endl; return 0;}
     }  
-    cout << nR1 << " " << nR2 << " " << nR3 << endl;    
+    cout << "Ending Resource : " << endl;
+    for(int i=0;i<R;i++) cout << nR[i] << " "; cout << endl;
 }
