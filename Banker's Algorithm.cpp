ac3be0662c9b64f6e1eee0f0b35456a4b02723ca
Abdul Al Mamun <124201619+mamunCSE64@users.noreply.github.com>
2023-11-13 08:14:05 +0600

    Update Banker's Algorithm.cpp

--- Banker's Algorithm.cpp
+++ Banker's Algorithm.cpp
@@ -4,63 +4,64 @@
 #define se second
 #define pb push_back
 using namespace std;
-
+// Banker's Algorithm
 int main()
 {        
     int P,R;
     cin>>P>>R;
     vector<int> iR(R);
     for(int i=0;i<R;i++) cin>>iR[i];
-    vector<int> maxN[P],allocR[P],currentR[P],available[P];
+    vector<int> maxN[P],allocR[P],currentRneed[P];
     for(int i=0;i<P;i++){
         for(int j=0;j<R;j++){
-            int x; cin>>x; 
+            int x; cin>>x; // max need
             maxN[i].pb(x);
         }
     }
-    vector<int> r(R);     
+    vector<int> rsum(R);     
     for(int i=0;i<P;i++){
         for(int j=0;j<R;j++){
             int x;cin>>x;
-            allocR[i].pb(x);
-            r[j]+=x;
+            allocR[i].pb(x); // allocated 
+            rsum[j]+=x; // total allcated of j'th
         }
     } 
     for(int i=0;i<P;i++){
         for(int j=0;j<R;j++){
             int d=maxN[i][j]-allocR[i][j];
-            currentR[i].pb(d);
+            currentRneed[i].pb(d);
         }         
     }
-    vector<int> nR(R);
+    vector<int> availableR(R);
     for(int i=0;i<R;i++){
-        nR[i]=iR[i]-r[i];
+        availableR[i]=iR[i]-rsum[i];
     }     
     int count=0;
-    vector<int> done(P,0);  
+    vector<int> done(P);  
     cout << "Initial Resource : " << endl;
     for(int i=0;i<R;i++) cout << iR[i] << " "; cout << endl;
+    
     while(count<P){
         bool deadlock=1;
         for(int i=0;i<P;i++){
             if(done[i]==0){
                 bool b=1;
                 for(int j=0;j<R;j++){
-                    if(currentR[i][j]>nR[j]) {
+                    if(currentRneed[i][j]>availableR[j]) {
                         b=0;break;
                     }
                 }
                 if(b){
                     for(int j=0;j<R;j++){
-                        nR[j]+=allocR[i][j];
+                        availableR[j]+=allocR[i][j];
                     }
                     done[i]=1,deadlock=0,count++;
-                    cout << "Safe State : P " << i << endl;
+                    cout << "Safe State : P" << i << endl;
                 }                 
             }
         }
-        if(deadlock==1){ cout << "DeadLock Created " << endl; return 0;}
+        if(deadlock==1){ cout << "DeadLock can be Created " << endl; return 0;}
     }  
     cout << "Ending Resource : " << endl;
-    for(int i=0;i<R;i++) cout << nR[i] << " "; cout << endl;
+    for(int i=0;i<R;i++) cout << availableR[i] << " "; cout << endl;
 }
