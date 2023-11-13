87e071ada787e6beace27e1da1eef237e9c99067
Abdul Al Mamun <124201619+mamunCSE64@users.noreply.github.com>
2023-11-13 10:14:43 +0600

    Update Semaphore Simple Code.cpp

--- Semaphore Simple Code.cpp
+++ Semaphore Simple Code.cpp
@@ -7,23 +7,25 @@ int signal(int s){
     return s;
 }
 int wait(int s){
-    while(s<=0);
+    while(s<1);
     s=s-1;
     return s;
 }
 int main()
 {         
-    int p[5],c[5],n=5;
-    for(int i=0;i<5;i++) p[i]=i;
-    for(int i=0;i<5;i++) c[i]=1;
+    int n;
+    cin>>n;
+    int p[n],c[n];
+    for(int i=0;i<n;i++) p[i]=i;
+    for(int i=0;i<n;i++) c[i]=1;
     for(int i=0;i<n-1;i++){
-        cout << "Think P: " << p[i] << endl;
+        cout << "Think P" << p[i] << endl;
         c[i]=wait(c[i]);        
     }
+    cout << "waiting/trap: P" << n-1 << endl;
     for(int i=n-2;i>=0;i--){        
-        cout << "Eat P: " << p[i] << endl;
+        cout << "Eat P" << p[i] << endl;
         c[i]=signal(c[i]);
-        if(i==n-2) cout << "Think P: " << n-1 << endl;
     }
-    cout << "Eat P: "<< p[n-1] << endl;
+    cout << "Eat P"<< p[n-1] << endl;
 }
