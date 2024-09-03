dcf99b58fdede5c391dff2c884bd023c4aeaf128
Abdul Al Mamun <124201619+mamunCSE64@users.noreply.github.com>
2024-09-03 11:57:42 +0600

    Update pancake_bfs.cpp

--- pancake_bfs.cpp
+++ pancake_bfs.cpp
@@ -11,47 +11,54 @@ int main()
     int n,range;
     cin>>n>>range;
     vector<int> v;
-    for(i=0;i<=n;i++){
-        int random_num=rand()%range+1;
+    for(i=1;i<=n;i++) {
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<int> dis(1, range);
+        int random_num = dis(gen);
         v.push_back(random_num);
-    }     
+    }  
     cout << "First array : " << endl;
     for(auto x: v) cout << x << " ";cout << endl << endl;
-    cout << "Will be sorted after reversing : " << endl;
     vector<int> result=v;
     sort(result.begin(),result.end());        
     map<vector<int>,vector<int>> index;
     map<vector<int>,int> visited;
     vector<vector<int>> ans;
     queue<vector<int>> q;
-
     q.push(v);       
     while(q.size()){ 
-        vector<int> t=q.front(); q.pop();
-        visited[t]=1;
+        vector<int> t=q.front(); q.pop();   
         if(t==result){
-            ans.push_back(index[t]);continue;
+            visited[result]=1;
+            ans.push_back(index[result]);
+            continue;
         }
         for(i=1;i<t.size();i++){
             vector<int> xx;
             for(j=i;j>=0;j--) xx.push_back(t[j]);
             for(j=i+1;j<t.size();j++) xx.push_back(t[j]);
             if(visited[xx]==0){
-                q.push(xx),visited[xx]=1;
+                visited[xx]=1;
                 index[xx]=index[t];
                 index[xx].push_back(i+1);
+                if(xx==result) ans.push_back(index[xx]);
+                else q.push(xx);
             }else if(xx==result){
                 index[result]=index[t]; index[result].push_back(i+1);
-                ans.push_back(index[xx]);
+                ans.push_back(index[result]);
             }
         }
     }        
+    cout << "Will be sorted after reversing to follow any step -> " << endl;
     for(auto x: ans){
+        cout << "Steps : ";
         if(x.size()==0){
-            cout << 0 << endl; continue;
-        }
-        for(auto x: x){
-            cout << x << " ";
+            cout << 0 << endl; 
+        }else{
+            for(auto x: x){
+                cout << x << " ";
+            }
         }
         cout << endl;
     }    
